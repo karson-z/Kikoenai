20d90c0bf5cbbf0c3b0c541f40f59b018c65b92b
karson <1840669143@qq.com>
2026-01-29 16:29:14 +0800

    fix：修复播放页返回手势问题，为播放页添加统一的手势拦截组件。

--- lib/core/widgets/layout/app_player_slider.dart
+++ lib/core/widgets/layout/app_player_slider.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // 必须引入，用于 SystemNavigator.pop
 import 'package:go_router/go_router.dart';
 import 'package:kikoenai/core/constants/app_constants.dart';
 import 'package:kikoenai/core/utils/data/other.dart';
 import 'package:kikoenai/core/widgets/layout/provider/main_scaffold_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// 请根据你的实际路径引入 BackButtonPriorityWrapper
+// import 'package:kikoenai/core/widgets/common/back_button_priority_wrapper.dart';
 import '../../theme/theme_view_model.dart';
+import '../common/back_button_interceptor.dart';
 import '../player/player_view.dart';
 import '../player/player_list_sheet.dart';
-import 'package:back_button_interceptor/back_button_interceptor.dart';
 import '../slider/sllding_up_panel_modify.dart';
 
 class SlidingPlayerPanel extends ConsumerStatefulWidget {
@@ -42,23 +45,6 @@ class _SlidingPlayerPanelState extends ConsumerState<SlidingPlayerPanel> {
   void initState() {
     super.initState();
     _panelController = widget.controller ?? PanelController();
-    BackButtonInterceptor.add(_backButtonInterceptor, zIndex: 1, name: 'PlayerPanel');
-  }
-
-  @override
-  void dispose() {
-    BackButtonInterceptor.remove(_backButtonInterceptor);
-    super.dispose();
-  }
-
-  bool _backButtonInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
-    final isExpanded = ref.read(mainScaffoldProvider).isPlayerExpanded;
-    if (isExpanded) {
-      debugPrint("拦截返回键：收起播放器");
-      _panelController.close();
-      return true;
-    }
-    return false;
   }
 
   @override
@@ -68,40 +54,57 @@ class _SlidingPlayerPanelState extends ConsumerState<SlidingPlayerPanel> {
     final isDark = ref.watch(explicitDarkModeProvider);
     final mainState = ref.watch(mainScaffoldProvider);
     final isMobile = MediaQuery.of(context).size.width < AppConstants.kMobileBreakpoint;
-    // 优化边界，让floatingButton 在任何设备上都能显示完全
+
     final paddingHeight = mainState.showBottomNav && !OtherUtil.isFullScreenPage(location)
         ? widget.minHeight + AppConstants.kAppBarHeight + 15
         : widget.minHeight;
     final safePadding = isMobile ? paddingHeight : 0.0;
 
-    return SlidingUpPanel(
-      controller: _panelController,
-      minHeight: widget.minHeight,
-      maxHeight: widget.maxHeight,
-      isDraggable: widget.isDraggable,
-      // 禁用淡入淡出, 基于源码修改
-      fadeCollapsed: false,
-      panelBuilder: (ScrollController sc, AnimationController controller) {
-        return MusicPlayerView(
-          dragProgressNotifier: controller,
-          panelController: _panelController,
-          minHeight: widget.minHeight,
-          onQueuePressed: () => PlayerPlaylistSheet.show(context, isDark: isDark),
-        );
+    return BackButtonPriorityWrapper(
+      // 必须小于 PlayerPlaylistSheet 的 100，否则弹窗无法优先关闭。
+      // 必须大于默认值 0，确保拦截系统默认返回。
+      zIndex: 10,
+      name: 'MainSlidingPlayer',
+      onBack: () {
+        // 1. 如果播放器展开，拦截并收起
+        if (mainState.isPlayerExpanded) {
+          debugPrint("PriorityWrapper: 收起播放器，拦截事件");
+          _panelController.close();
+          return true;
+        }
+
+        // 2. 如果播放器没展开，不拦截
+        debugPrint("PriorityWrapper: 放行，交由系统路由处理");
+        return false;
       },
 
-      body: Padding(
-        padding: EdgeInsets.only(bottom: safePadding),
-        child: widget.body,
+      child: SlidingUpPanel(
+        controller: _panelController,
+        minHeight: widget.minHeight,
+        maxHeight: widget.maxHeight,
+        isDraggable: widget.isDraggable,
+        fadeCollapsed: false,
+        panelBuilder: (ScrollController sc, AnimationController controller) {
+          return MusicPlayerView(
+            dragProgressNotifier: controller,
+            panelController: _panelController,
+            minHeight: widget.minHeight,
+            onQueuePressed: () => PlayerPlaylistSheet.show(context, isDark: isDark),
+          );
+        },
+        body: Padding(
+          padding: EdgeInsets.only(bottom: safePadding),
+          child: widget.body,
+        ),
+        onPanelOpened: () {
+          mainController.expandPlayer();
+          mainController.setBottomNav(false);
+        },
+        onPanelClosed: () {
+          mainController.collapsePlayer();
+          mainController.setBottomNav(true);
+        },
       ),
-      onPanelOpened: () {
-        mainController.expandPlayer();
-        mainController.setBottomNav(false);
-      },
-      onPanelClosed: () {
-        mainController.collapsePlayer();
-        mainController.setBottomNav(true);
-      },
     );
   }
 }
