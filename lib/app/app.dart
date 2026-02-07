d0ea5f1c926558d3aab223a27df5c3370717a584
karson-z <1840669143@qq.com>
2026-02-08 00:07:23 +0800

    feat(lyrics): 修改第三方包，实现类似AM的延迟动画效果

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kikoenai/config/app_version_config.dart';
 import 'package:kikoenai/core/routes/app_router.dart';
 import 'package:kikoenai/core/theme/app_theme.dart';
-import 'package:kikoenai/core/constants/app_constants.dart';
 import 'package:kikoenai/core/widgets/scroll/my_scroll_behavior.dart';
 
 import '../core/theme/theme_view_model.dart';
