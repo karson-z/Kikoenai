659ad79bc07301e4219de781f8154c5048719cf6
karson-z <1840669143@qq.com>
2026-04-19 14:45:15 +0800

    feat(version): change version

--- lib/config/app_version_config.dart
+++ lib/config/app_version_config.dart
@@ -1,6 +1,6 @@
 class VersionConfig{
   /// 当前版本
-  static const String version = '1.0.2';
+  static const String version = '1.0.3';
   /// app名称
   static const String appName = 'Kikoenai';
   /// Github 项目主页
