6381bc587aae7884c36ab5b43386eb274daab586
karson-z <1840669143@qq.com>
2026-05-03 23:01:04 +0800

    fix(image): 更新了图片库的依赖，将其迁移至社区版本，共同享受Hive带来的高性能 收拢了所有散落在外面的CachedNetworkImage

--- lib/core/utils/data/colors_util.dart
+++ lib/core/utils/data/colors_util.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:cached_network_image_ce/cached_network_image.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:palette_generator_master/palette_generator_master.dart';
 
 class ColorUtils {
