20145eea8cbe6087f00380dd0c6ccfa42e36e396
karson-z <1840669143@qq.com>
2026-05-14 22:39:32 +0800

    feat(detail): 由于相似作品接口暂时不可用，使用占位数据源进行替代

--- lib/core/widgets/card/work_single_col_card.dart
+++ lib/core/widgets/card/work_single_col_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:kikoenai/core/widgets/image_box/simple_extended_image.dart';
 
 import '../../../features/album/data/model/work.dart';
 import '../../../features/album/presentation/widget/work_tag.dart';
 import '../../enums/tag_enum.dart';
+import '../../routes/app_routes.dart';
 
 class WorkSingleColCard extends StatelessWidget{
   final Work work;
@@ -12,28 +14,34 @@ class WorkSingleColCard extends StatelessWidget{
   const WorkSingleColCard({super.key,required this.work});
   @override
   Widget build(BuildContext context) {
-     return Container(
-       padding: const EdgeInsets.all(12),
-       child: Row(
-         children: [
-           SimpleExtendedImage(
-             height: 72,
-             width: 72,
-             work.mainCoverUrl ?? ''
-           ),
-           Expanded(
-             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text(work.title ?? '',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 13)),
-                 const SizedBox(height: 4),
-                 TagRow(tags: work.vas ?? [], type: TagType.va),
-                 const SizedBox(height: 4),
-                 TagRow(tags: work.tags ?? [], type: TagType.tag),
-               ],
+     return GestureDetector(
+       onTap: () {
+       context.push(AppRoutes.detail, extra: {'work': work,'isLocal': false});
+     },
+       child: Container(
+         padding: const EdgeInsets.all(12),
+         child: Row(
+           children: [
+             SimpleExtendedImage(
+                 height: 72,
+                 width: 72,
+                 work.mainCoverUrl ?? ''
              ),
-           )
-         ],
+             const SizedBox(width: 12),
+             Expanded(
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text(work.title ?? '',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 13,),maxLines: 1,),
+                   const SizedBox(height: 4),
+                   TagRow(tags: work.vas ?? [], type: TagType.va),
+                   const SizedBox(height: 4),
+                   TagRow(tags: work.tags ?? [], type: TagType.tag),
+                 ],
+               ),
+             )
+           ],
+         ),
        ),
      );
   }
