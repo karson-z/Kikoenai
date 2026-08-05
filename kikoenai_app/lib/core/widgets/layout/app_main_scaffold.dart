324cecfddb58b10259080dff08239e3b798965b8
zhangkaishan <>
2026-08-05 14:05:11 +0800

    feat:当前播放器没有音频的时候不会出现迷你播放器

--- kikoenai_app/lib/core/widgets/layout/app_main_scaffold.dart
+++ kikoenai_app/lib/core/widgets/layout/app_main_scaffold.dart
@@ -9,6 +9,7 @@ import 'package:kikoenai/core/widgets/layout/provider/main_scaffold_provider.dar
 import 'package:kikoenai/config/navigation_item.dart';
 import 'package:kikoenai/core/widgets/layout/navigation_rail.dart';
 import 'package:kikoenai/core/widgets/layout/adaptive_app_bar.dart';
+import 'package:kikoenai/features/player/provider/player_controller_provider.dart';
 import '../../../features/player/page/player_view.dart';
 import '../common/back_button_interceptor.dart';
 import '../slider/player_sheet_panel.dart';
@@ -59,6 +60,14 @@ class _MainScaffoldState extends ConsumerState<MainScaffold> {
     final mainController = ref.watch(mainScaffoldProvider.notifier);
     final mainState = ref.watch(mainScaffoldProvider);
     final panelController = ref.watch(panelControllerProvider);
+    final hasPlaybackItems = ref.watch(
+      playerControllerProvider.select(
+        (state) => state.playbackQueue.isNotEmpty,
+      ),
+    );
+    final miniPlayerHeight = hasPlaybackItems
+        ? AppConstants.kMiniPlayerHeight
+        : 0.0;
 
     Widget bodyContent;
     if (isMobile) {
@@ -102,7 +111,7 @@ class _MainScaffoldState extends ConsumerState<MainScaffold> {
         },
         child: PlayerSheetPanel(
           controller: panelController,
-          minHeight: AppConstants.kMiniPlayerHeight,
+          minHeight: miniPlayerHeight,
           maxHeight: MediaQuery.sizeOf(context).height,
           fadeCollapsed: false,
           panelBuilder: (ScrollController sc, AnimationController controller) {
